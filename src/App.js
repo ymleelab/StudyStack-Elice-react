f17ac385433946ee4fa4c00c96b626da0795f8ce
ymleelab <ymleelab@gmail.com>
2022-06-08 14:54:21 +0900

    Add mode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
 
-function Header() {
+function Header(props) {
   return (
     <header>
       <h1>
-        <a href="/">WWW</a>
+        <a
+          href="/"
+          onClick={(evt) => {
+            //console.log(evt);
+            evt.preventDefault();
+            props.onSelect();
+          }}
+        >
+          WWW
+        </a>
       </h1>
     </header>
   );
@@ -14,7 +25,15 @@ function Nav(props) {
   const list = props.data.map((e) => {
     return (
       <li key={e.id}>
-        <a href={"/read/" + e.id}>{e.title}</a>
+        <a
+          href={"/read/" + e.id}
+          onClick={(evt) => {
+            evt.preventDefault();
+            props.onSelect(e.id);
+          }}
+        >
+          {e.title}
+        </a>
       </li>
     );
   });
@@ -33,15 +52,58 @@ function Article(props) {
   );
 }
 function App() {
+  const [mode, setMode] = useState("WELCOME");
+  const [id, setId] = useState(null);
+
   const topics = [
     { id: 1, title: "html", body: "html is ..." },
     { id: 2, title: "css", body: "css is ..." },
   ];
+  let content = null;
+  if (mode === "WELCOME") {
+    content = <Article title="Welcome" body="Hello, WEB!"></Article>;
+  } else if (mode === "READ") {
+    const topic = topics.filter((e) => {
+      if (e.id === id) {
+        return true;
+      } else {
+        return false;
+      }
+    })[0];
+    //console.log(topic);
+    content = <Article title={topic.title} body={topic.body}></Article>;
+  }
+
+  function createHandler() {
+    alert("create!");
+  }
   return (
     <div>
-      <Header></Header>
-      <Nav data={topics}></Nav>
-      <Article title="Welcome" body="Hello, WEB!"></Article>
+      <Header
+        onSelect={() => {
+          setMode("WELCOME");
+          //alert("header");
+        }}
+      ></Header>
+      <Nav
+        data={topics}
+        onSelect={(id) => {
+          setMode("READ");
+          setId(id);
+          //alert("nav" + id);
+        }}
+      ></Nav>
+      {content}
+      <ButtonGroup
+        variant="contained"
+        aria-label="outlined primary button group"
+      >
+        <Button variant="outlined" onClick={createHandler}>
+          Create
+        </Button>
+        <Button variant="outlined">Update</Button>
+      </ButtonGroup>
+      <Button variant="outlined">Delete</Button>
     </div>
   );
 }
